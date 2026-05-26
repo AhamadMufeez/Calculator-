026995485c982a475e1ec4736ee3ff5de8977b81
Ahmad Mufeez <mufeezahmad938@gmail.com>
2026-05-26 12:09:14 +0530

    Update main.js
    gonna have the app offline

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ function hasDecimal(str) {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((reg) => console.log("SW registered:", reg.scope))
       .catch((err) => console.log("SW failed:", err));
   });
